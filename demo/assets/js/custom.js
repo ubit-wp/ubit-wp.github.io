7ce4af8bd8ae67244e6334327d75a0cf0a3ff05c
Mantas <m.zilius@gmail.com>
2019-07-16 15:43:49 +0300

    Upload new demo landing page

---
+++ demo/assets/js/custom.js
@@ -0,0 +1,121 @@
+// JavaScript Document
+
+
+	$(window).on('load', function() {
+
+		"use strict";
+
+		$(window).stellar({});
+
+	});
+
+
+	$(document).ready(function() {
+
+		"use strict";
+
+		/*----------------------------------------------------*/
+		/*	Animated Scroll To Anchor
+		/*----------------------------------------------------*/
+
+		$('.header a[href^="#"], #page a.btn[href^="#"]').on('click', function (e) {
+
+			e.preventDefault();
+
+			var target = this.hash,
+				$target = jQuery(target);
+
+			$('html, body').stop().animate({
+				'scrollTop': $target.offset().top - 110 // - 200px (nav-height)
+			}, 'slow', 'easeInSine', function () {
+				window.location.hash = '1' + target;
+			});
+
+		});
+
+
+		/*----------------------------------------------------*/
+		/*	ScrollUp
+		/*----------------------------------------------------*/
+
+		$.scrollUp = function (options) {
+
+			// Defaults
+			var defaults = {
+				scrollName: 'scrollUp', // Element ID
+				topDistance: 600, // Distance from top before showing element (px)
+				topSpeed: 800, // Speed back to top (ms)
+				animation: 'fade', // Fade, slide, none
+				animationInSpeed: 200, // Animation in speed (ms)
+				animationOutSpeed: 200, // Animation out speed (ms)
+				scrollText: '', // Text for element
+				scrollImg: false, // Set true to use image
+				activeOverlay: false // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+			};
+
+			var o = $.extend({}, defaults, options),
+				scrollId = '#' + o.scrollName;
+
+			// Create element
+			$('<a/>', {
+				id: o.scrollName,
+				href: '#top',
+				title: o.scrollText
+			}).appendTo('body');
+
+			// If not using an image display text
+			if (!o.scrollImg) {
+				$(scrollId).text(o.scrollText);
+			}
+
+			// Minium CSS to make the magic happen
+			$(scrollId).css({'display':'none','position': 'fixed','z-index': '2147483647'});
+
+			// Active point overlay
+			if (o.activeOverlay) {
+				$("body").append("<div id='"+ o.scrollName +"-active'></div>");
+				$(scrollId+"-active").css({ 'position': 'absolute', 'top': o.topDistance+'px', 'width': '100%', 'border-top': '1px dotted '+o.activeOverlay, 'z-index': '2147483647' });
+			}
+
+			// Scroll function
+			$(window).on('scroll', function(){
+				switch (o.animation) {
+					case "fade":
+						$( ($(window).scrollTop() > o.topDistance) ? $(scrollId).fadeIn(o.animationInSpeed) : $(scrollId).fadeOut(o.animationOutSpeed) );
+						break;
+					case "slide":
+						$( ($(window).scrollTop() > o.topDistance) ? $(scrollId).slideDown(o.animationInSpeed) : $(scrollId).slideUp(o.animationOutSpeed) );
+						break;
+					default:
+						$( ($(window).scrollTop() > o.topDistance) ? $(scrollId).show(0) : $(scrollId).hide(0) );
+				}
+			});
+
+			// To the top
+			$(scrollId).on('click', function(event){
+				$('html, body').animate({scrollTop:0}, o.topSpeed);
+				event.preventDefault();
+			});
+
+		};
+
+		$.scrollUp();
+
+
+		/*----------------------------------------------------*/
+		/*	Reviews Grid
+		/*----------------------------------------------------*/
+
+		$('.grid-loaded').imagesLoaded(function () {
+	        var $grid = $('.masonry-wrap').isotope({
+	            itemSelector: '.review',
+	            percentPosition: true,
+	            transitionDuration: '0.7s',
+	            masonry: {
+	              columnWidth: '.review',
+	            }
+	        });
+	    });
+
+
+	});
